dfeed8a5cf50ca2e7b9bf93192f9c75e4936d613
BadrinathM6 <gopin5131@gmail.com>
2024-08-13 17:07:17 +0530

    feat : created a java program to calculate the arithmetic operation

---
+++ Calculator.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+
+public class Calculator {
+  
+ // Building a method to convert the string into int 
+   
+  public static int validateAndConvertToInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            return Integer.MIN_VALUE; 
+        }
+    }
+    
+  public static void main(String[] args) {
+   
+   // getting the input from the terminal using built-in class Scanner by calling it with the name of 'scanner' object 
+    
+    Scanner scanner = new Scanner(System.in);
+    
+   //printing and handling the error 
+   
+    System.out.println("Enter one number to perform these(+,-,*,/)" );
+    String input1 = scanner.nextLine();
+    
+    int number1 = validateAndConvertToInt(input1);
+    if(number1 == Integer.MIN_VALUE){
+      
+      System.out.println(" ! Please enter only numbers");
+    }
+    
+    
+    System.out.println("Enter these operator(+,-,*,/) to perform the operation" );
+    char operator = scanner.next().charAt(0);
+    
+    
+    System.out.println("Enter another number to perform these(+,-,*,/)" );
+    String input2 = scanner.nextLine();
+    
+    int number2 = validateAndConvertToInt(input2);
+    if(number2 == Integer.MIN_VALUE){
+      
+      System.out.println(" ! Please enter only numbers"); 
+    }
+    
+   
+   // setting the operator to perform these operation !!
+   
+    int finalResult = 0;
+    
+    
+    
+    if(operator == '+'){
+      
+      finalResult = number1 + number2 ;
+      System.out.println(finalResult);
+    }
+    
+    else if(operator == '-'){
+      
+      finalResult = number1 - number2 ;
+      System.out.println(finalResult);
+    }
+    
+    else if(operator == '*'){
+      
+      finalResult = number1 * number2 ;
+      System.out.println(finalResult);
+    }
+    
+    else if(operator == '/'){
+      
+      finalResult = number1 / number2 ;
+      System.out.println(finalResult);
+    }
+    
+    
+    else{
+        
+        System.out.println("invalid operator ! Please enter valid operator ");
+    
+    }
+    
+     
+  }
+  
+  
+}
